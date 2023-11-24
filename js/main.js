1157b947ee1bd07f513149de5b72c61354d921f0
Bruno <brunoamagar@campus.monlau.com>
2023-11-24 19:28:47 +0100

    general-updates

--- js/main.js
+++ js/main.js
@@ -27,11 +27,18 @@ const dayWeek = weekday[date.getDay()]; //Get week day literal
 //#endregion
 
 
-//INITIALLY DISPLAY THE DATE & TIME
+//#region VARS DEFINITON
+var canvas, ctx;
+//#endregion
+
+
+//#region HIDE INITIALS
 actualDate.innerHTML = day + '-' + month + '-' + year;
 actualTime.innerHTML = hours + ':' + minutes + ':' + seconds;
+//#endregion
 
 
+//#region FUNCTIONS
 //MULTIPLE SELECTION FOR DIVS
 function selectDateFormat(selectedOpt) {
     selections.forEach(sel => sel.classList.remove('selected'));
@@ -41,9 +48,113 @@ function selectHourFormat(selectedOpt) {
     selections.forEach(sel => sel.classList.remove('selected2'));
     selectedOpt.classList.add('selected2');
 }
+//FUNCTION FOR DRAW CLOCK
+function draw() {
+    let time = (function () {
+        let midnight = date;
+        midnight.setHours(0);
+        midnight.setMinutes(0);
+        midnight.setSeconds(0);
+        midnight.setMilliseconds(0);
+        return Date.now() - midnight.getTime();
+    })(),
+        hours = time / (60 * 60 * 1000),
+        minutes = hours * 60 % 60,
+        seconds = minutes * 60 % 60,
+        c = { x: canvas.width / 2, y: canvas.height / 2 };
+
+    ctx.clearRect(0, 0, canvas.width , canvas.height);
+    ctx.lineCap = 'round';
+
+    secondHand();
+    minuteHand();
+    hourHand();
+    face();
+
+    function face() {
+        //Border clock
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = 'black';
+        ctx.beginPath();
+        ctx.arc(c.x, c.y, 140, 0, Math.PI * 2);
+        ctx.stroke();
+    
+        //Dashes Clock
+        ctx.lineWidth = 4;
+        for (let i = 0; i < 60; i++) {
+            let r = 135,
+            l = 5;
+            ctx.strokeStyle = 'rgba(0, 0, 0, 0.25)';
+            if (i % 5 === 0) 
+                r -= l,
+                l += 2,
+                ctx.strokeStyle = 'rgba(0, 0, 0, 0.5)';
+            let v = new Vector(r, Math.PI * 2 * (i / 60) - Math.PI / 2);
+            ctx.beginPath();
+            ctx.moveTo(v.getX() + c.x, v.getY() + c.y);
+            v.setMag(r + l);
+            ctx.lineTo(v.getX() + c.x, v.getY() + c.y);
+            ctx.stroke();
+        }
+
+        //Numbers
+        ctx.font = '20px Rubik';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        for (let i = 1; i <= 12; i++) {
+            let v = new Vector(113, Math.PI * 2 * (i / 12) - Math.PI / 2);
+            ctx.fillText(i, v.getX() + c.x, v.getY() + c.y);
+            
+        }
+    
+    }
+
+    function secondHand() {
+        ctx.lineWidth = 1.5;
+        ctx.strokeStyle = 'red';
+        ctx.beginPath();
+        let a = Math.PI * 2 * (seconds / 60) - Math.PI / 2;
+        let v = new Vector(95, a);
+        ctx.moveTo(c.x, c.y);
+        ctx.lineTo(v.getX() + c.x, v.getY() + c.y);
+        ctx.stroke();
+    }
+
+    function minuteHand() {
+        ctx.lineWidth = 3.5;
+        ctx.strokeStyle = 'black';
+        ctx.beginPath();
+        let a = Math.PI * 2 * (minutes / 60) - Math.PI / 2;
+        let v = new Vector(95, a);
+        ctx.moveTo(c.x, c.y);
+        ctx.lineTo(v.getX() + c.x, v.getY() + c.y);
+        ctx.stroke();
+    }
+
+    function hourHand() {
+        ctx.lineWidth = 3.5;
+        ctx.strokeStyle = 'black';
+        ctx.beginPath();
+        let a = Math.PI * 2 * (hours / 12) - Math.PI / 2;
+        let v = new Vector(60, a);
+        ctx.moveTo(c.x, c.y);
+        ctx.lineTo(v.getX() + c.x, v.getY() + c.y);
+        ctx.stroke();
+    }
+
+}
+function init() {
+    canvas = document.getElementById("clock");
+    canvas.width = canvas.height = 300;
+    ctx = canvas.getContext('2d');
+
+    setInterval(draw, 10);
+}
+init();
+//#endregion
 
 
-//#region LISTENERS FOR USER SELECTION
+//#region LISTENERS
 normalDate.addEventListener("click", function () {
     actualDate.innerHTML = day + '/' + month + '/' + year;
 });
@@ -54,14 +165,18 @@ simplifiedDate.addEventListener("click", function () {
     actualDate.innerHTML = dayWeek + ', ' + day + ' of ' + monthLiteral + ' of ' + year;
 });
 twelveSelect.addEventListener("click", function () {
-    var AMPM = hours >= 12 ? 'PM' : 'AM';
+    var ampm = hours >= 12 ? 'PM' : 'AM';
 
     if (hours === 12) {
         hours = 12;
     } else {
-        hours = hours%12;
+        hours = hours % 12;
     }
 
-    actualTime.innerHTML = hours + ':' + minutes + ':' + seconds + ' ' + AMPM;
+    actualTime.innerHTML = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
 });
+twentySelect.addEventListener("click", function () {
+    actualTime.innerHTML = hours + ':' + minutes + ':' + seconds;
+});
+
 //#endregion
